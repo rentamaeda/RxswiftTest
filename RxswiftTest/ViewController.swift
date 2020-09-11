fef5b0b15a1bb78a74d88971abfab19ce2c18b5c
rentamaeda <japan_lunch_fate@yahoo.co.jp>
2020-09-11 11:22:50 +0900

    Rx Swiftでサンプル ボタンがタップされるたびにカウントされる

--- RxswiftTest/ViewController.swift
+++ RxswiftTest/ViewController.swift
@@ -7,14 +7,31 @@
 //
 
 import UIKit
-
+import RxSwift
+import RxCocoa
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var favButton: UIButton!
+    @IBOutlet weak var favCountLabel: UILabel!
+    
+    private var favCount = 0
+    private let disposeBag = DisposeBag()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //rx.tap ボタンがタップされたら通知
+        favButton.rx.tap
+            //subscribe　 favButton.rx.tapされたら呼ばれる
+            //onNextイベントがあるたび呼ばれる
+            .subscribe(onNext: {[unowned self] _ in
+                self.favCount += 1
+                self.favCountLabel.text = String(self.favCount)
+            })
+            .disposed(by: disposeBag)
+        
     }
-
-
+    
+    
 }
 
